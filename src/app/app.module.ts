eca7799da08bec94c40d42dadbc248e70d85fcca
Muhammad Akmal <akmal@emojiit.com>
2022-03-26 19:48:52 +0200

    'effects_added'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -29,6 +29,7 @@ import { NotfoundComponent } from './components/shared/notfound/notfound.compone
 import * as fromAuth from './States/Auth_State/Reducer/auth-reducer';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { environment } from '../environments/environment';
+import { EffectsModule } from '@ngrx/effects';
 registerLocaleData(en);
 
 @NgModule({
@@ -59,7 +60,8 @@ registerLocaleData(en);
     HttpClientModule,
     BrowserAnimationsModule,
     NGZORROModule,
-    // StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }),
+    // EffectsModule.forRoot([]),
   ],
   providers: [{ provide: NZ_I18N, useValue: en_US }],
   bootstrap: [AppComponent],
