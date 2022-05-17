41e8b66803514fd291f3ab89d1ab6a68a878d42a
Muhammad Akmal <akmal@emojiit.com>
2022-05-17 17:56:37 +0200

    animejs added

--- src/app/components/profile/login/login.component.ts
+++ src/app/components/profile/login/login.component.ts
@@ -1,21 +1,41 @@
 import { Component, OnInit } from '@angular/core';
+import anime from 'animejs'; 
 declare var $: any;
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
-    $("#login-button").click(function(event: any){
-      event.preventDefault();
+    var design = anime({
+      targets: 'svg #XMLID5',
+      keyframes: [
+        {translateX: -500},
+        {rotateY: 180},
+        {translateX: 920},
+        {rotateY: 0},
+        {translateX: -500},
+        {rotateY: 180},
+        {translateX: -500},
+      ],
+      easing: 'easeInOutSine',
+      duration: 60000,
+    });
     
-    $('form').fadeOut(500);
-    $('.wrapper').addClass('form-success');
- });
+    anime({
+      targets: '#dust-paarticle path',
+      translateY: [10, -150],
+      direction: 'alternate',
+      loop: true,
+      delay: function(el, i, l) {
+        return i * 100;
+      },
+      endDelay: function(el, i, l) {
+        return (l - i) * 100;
+      }
+    });
   }
-
 }
