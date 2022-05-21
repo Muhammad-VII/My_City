77097909591e96862c8f75dc4056228e9e16e251
Muhammad Akmal <akmal@emojiit.com>
2022-05-21 19:06:34 +0200

    'optimizaiton'

--- src/app/components/main/contact-us/contact-us.component.ts
+++ src/app/components/main/contact-us/contact-us.component.ts
@@ -7,23 +7,21 @@ import { animate, style, transition, trigger } from '@angular/animations';
   templateUrl: './contact-us.component.html',
   styleUrls: ['./contact-us.component.scss'],
   animations: [
-    trigger(
-      'enterAnimation', [
-        transition(':enter', [
-          style({transform: 'translateX(100%)', opacity: 0}),
-          animate('500ms', style({transform: 'translateX(0)', opacity: 1}))
-        ]),
-        transition(':leave', [
-          style({transform: 'translateX(0)', opacity: 1}),
-          animate('500ms', style({transform: 'translateX(100%)', opacity: 0}))
-        ])
-      ]
-    )
+    trigger('enterAnimation', [
+      transition(':enter', [
+        style({ transform: 'translateX(100%)', opacity: 0 }),
+        animate('500ms', style({ transform: 'translateX(0)', opacity: 1 })),
+      ]),
+      transition(':leave', [
+        style({ transform: 'translateX(0)', opacity: 1 }),
+        animate('500ms', style({ transform: 'translateX(100%)', opacity: 0 })),
+      ]),
+    ]),
   ],
 })
 export class ContactUsComponent implements OnInit {
   pageTitle: string = 'Contact Us';
-  messageRecevied: boolean = false
+  messageRecevied: boolean = false;
   constructor() {}
   contactForm: FormGroup = new FormGroup({
     name: new FormControl('', [Validators.required, Validators.minLength(3)]),
@@ -39,11 +37,20 @@ export class ContactUsComponent implements OnInit {
   });
 
   submitContactForm() {
-    this.messageRecevied = true;
-    this.contactForm.reset();
-    setTimeout(() => {
-      this.messageRecevied = false;
-    }, 5000)
+    if (this.contactForm.valid) {
+      this.messageRecevied = true;
+      this.contactForm.reset();
+      setTimeout(() => {
+        this.messageRecevied = false;
+      }, 5000);
+    } else {
+      Object.values(this.contactForm.controls).forEach((control) => {
+        if (control.invalid) {
+          control.markAsDirty();
+          control.updateValueAndValidity({ onlySelf: true });
+        }
+      });
+    }
   }
   ngOnInit(): void {}
 }
