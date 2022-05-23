41e94404cf18357831bcfea37c6f6e2e650ea47e
Muhammad Akmal <akmal@emojiit.com>
2022-05-23 14:45:57 +0200

    offers done

--- src/app/components/offers/offers.component.ts
+++ src/app/components/offers/offers.component.ts
@@ -7,9 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OffersComponent implements OnInit {
   pageTitle: string = 'Offers';
+  offers: any[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   constructor() { }
   checked: boolean = false;
+  openFilter(): void {
+    $('.side-filter').slideToggle(300)
+  }
   ngOnInit(): void {
+    
   }
 
 }
