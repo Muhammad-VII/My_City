a48c9fb9fd7cff0d4b6ea6ac01074966ced899ca
Muhammad Akmal <akmal@emojiit.com>
2022-05-28 03:26:51 +0200

    dark mode added for story page

--- src/app/components/main/about-us/about-us.component.ts
+++ src/app/components/main/about-us/about-us.component.ts
@@ -9,6 +9,10 @@ export class AboutUsComponent implements OnInit {
 
   constructor() { }
   pageTitle: string = 'Get to know us';
+  swithToDark(): void {
+    const about_section: HTMLDivElement = document.querySelector('.about-story')! as HTMLDivElement;
+    about_section.classList.toggle("dark")
+  }
   ngOnInit(): void {
   }
 
