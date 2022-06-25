34a6603d254e3f5e7574d94ed5f476176dddfd1b
Muhammad Akmal <akmal@emojiit.com>
2022-06-25 21:28:26 +0200

    'explore'

--- src/app/components/main/explore/explore.component.ts
+++ src/app/components/main/explore/explore.component.ts
@@ -1,13 +1,15 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-declare const ol:any;
+declare const ol: any;
 @Component({
   selector: 'app-explore',
   templateUrl: './explore.component.html',
   styleUrls: ['./explore.component.scss'],
-  encapsulation: ViewEncapsulation.None
+  encapsulation: ViewEncapsulation.None,
 })
 export class ExploreComponent implements OnInit {
-  pageTitle:string = 'Explore'
+  pageTitle: string = 'Explore';
+  showStepper = true;
+
   constructor() {
     // var map = new ol.Map({
     //   target: 'map',
@@ -23,49 +25,92 @@ export class ExploreComponent implements OnInit {
     // });
   }
 
-  ngOnInit(): void {
+  distrects: any[] = [
+    {
+      imgUrl: 'tromso-forsidebilde-vinter-1-scaled.jpg',
+      title: "Distrect title",
+      describtion: `
+      The weather in Trondheim For more than a thousand years, Trondheim has been a center for development of science, culture, infrastructure and innovation.
+      `,
+      link: 'https://www.trondheim.no/',
+    },
+
+    {
+      imgUrl: 'bergen-frontpage-2560x1706-1.jpg',
+      title: "Distrect title",
+      describtion: `
+      The weather in Trondheim For more than a thousand years, Trondheim has been a center for development of science, culture, infrastructure and innovation.
+      `,
+      link: 'https://www.trondheim.no/',
+    },
+
+    {
+      imgUrl: 'asker-frontpage-2560x1464-1.jpg',
+      title: "Distrect title",
+      describtion: `
+      The weather in Trondheim For more than a thousand years, Trondheim has been a center for development of science, culture, infrastructure and innovation.
+      `,
+      link: 'https://www.trondheim.no/',
+    },
+
+    {
+      imgUrl: 'bergen-frontpage-2560x1706-1.jpg',
+      title: "Distrect title",
+      describtion: `
+      The weather in Trondheim For more than a thousand years, Trondheim has been a center for development of science, culture, infrastructure and innovation.
+      `,
+      link: 'https://www.trondheim.no/',
+    },
+
+    {
+      imgUrl: 'forsidebilde-stavanger-2560x1831-1.jpg',
+      title: "Distrect title",
+      describtion: `
+      The weather in Trondheim For more than a thousand years, Trondheim has been a center for development of science, culture, infrastructure and innovation.
+      `,
+      link: 'https://www.trondheim.no/',
+    },
+    {
+      imgUrl: 'forsidebilde-stavern-citadellet-2560x1831-1.jpg',
+      title: "Distrect title",
+      describtion: `
+      The weather in Trondheim For more than a thousand years, Trondheim has been a center for development of science, culture, infrastructure and innovation.
+      `,
+      link: 'https://www.trondheim.no/',
+    },
+
+    {
+      imgUrl: 'kristiansandcity.jpg',
+      title: "Distrect title",
+      describtion: `
+      The weather in Trondheim For more than a thousand years, Trondheim has been a center for development of science, culture, infrastructure and innovation.
+      `,
+      link: 'https://www.trondheim.no/',
+    },
+
+    {
+      imgUrl: 'oslo-aker-brygge-clock-tower-2600x873-1-scaled.jpg',
+      title: "Distrect title",
+      describtion: `
+      The weather in Trondheim For more than a thousand years, Trondheim has been a center for development of science, culture, infrastructure and innovation.
+      `,
+      link: 'https://www.trondheim.no/',
+    },
+
+    {
+      imgUrl: 'oslo-akershus-festning-scaled.jpg',
+      title: "Distrect title",
+      describtion: `
+      The weather in Trondheim For more than a thousand years, Trondheim has been a center for development of science, culture, infrastructure and innovation.
+      `,
+      link: 'https://www.trondheim.no/',
+    },
+  ];
+
+  scrollDown():void {
+    const articleOffset: number = document.getElementById('explore-section')!.offsetTop;
+    window.scrollTo(0, articleOffset);
   }
 
-  current = 0;
-
-  index = 'First-content';
-
-  pre(): void {
-    this.current -= 1;
-    this.changeContent();
-  }
-
-  next(): void {
-    this.current += 1;
-    this.changeContent();
-  }
-
-  done(): void {
-    console.log('done');
-  }
-
-  changeContent(): void {
-    switch (this.current) {
-      case 0: {
-        this.index = 'First-content';
-        break;
-      }
-      case 1: {
-        this.index = 'Second-content';
-        break;
-      }
-      case 2: {
-        this.index = 'third-content';
-        break;
-      }
-
-      case 3: {
-        this.index = 'fourth-content';
-        break;
-      }
-      default: {
-        this.index = 'error';
-      }
-    }
-  }
+  ngOnInit(): void {}
 }
