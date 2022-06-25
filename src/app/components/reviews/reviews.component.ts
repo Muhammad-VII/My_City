7a44978a824e8102549e32a11b720fc5baf0728d
Muhammad Akmal <akmal@emojiit.com>
2022-06-25 04:11:55 +0200

    'reviews_added'

--- src/app/components/reviews/reviews.component.ts
+++ src/app/components/reviews/reviews.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-reviews',
   templateUrl: './reviews.component.html',
-  styleUrls: ['./reviews.component.scss']
+  styleUrls: ['./reviews.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class ReviewsComponent implements OnInit {
-  pageTitle: string = `Explore`
-  constructor() { }
+  pageTitle: string = `Explore`;
+  reviews: any[] = [
+    {
+      name: 'John Doe',
+      review:
+        'This application is designed to help you to learn the basics of the English language. It will help you to understand the meaning of the words and phrases, and to practice the correct usage of the words and phrases. It will also help you to improve your English language skills.',
+    },
+    {
+      name: 'John Doe',
+      review:
+        'This application is designed to help you to learn the basics of the English language. It will help you to understand the meaning of the words and phrases, and to practice the correct usage of the words and phrases. It will also help you to improve your English language skills.',
+    },
+    {
+      name: 'John Doe',
+      review:
+        'This application is designed to help you to learn the basics of the English language. It will help you to understand the meaning of the words and phrases, and to practice the correct usage of the words and phrases. It will also help you to improve your English language skills.',
+    },
+    {
+      name: 'John Doe',
+      review:
+        'This application is designed to help you to learn the basics of the English language. It will help you to understand the meaning of the words and phrases, and to practice the correct usage of the words and phrases. It will also help you to improve your English language skills.',
+    },
+  ];
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
